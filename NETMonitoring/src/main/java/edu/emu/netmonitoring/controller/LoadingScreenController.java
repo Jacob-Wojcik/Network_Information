d8eb25fcc70ee97bd1557d63730f5204919ea1d2
jacob-a-wojcik <jwojcik1@emich.edu>
2024-04-15 19:51:46 -0400

    updated panel sizing, added loading screen with styling, and small other things.

---
+++ NETMonitoring/src/main/java/edu/emu/netmonitoring/controller/LoadingScreenController.java
@@ -0,0 +1,16 @@
+package edu.emu.netmonitoring.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+
+public class LoadingScreenController {
+
+    @FXML
+    private StackPane root;
+
+    // You can add any initialization logic here if needed
+    @FXML
+    private void initialize() {
+        // Add initialization code here if necessary
+    }
+}
